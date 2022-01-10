02368743ae99cfe2970c524c500cfb9c1b0586d5
Johan Muñetón <johanmudea@gmail.com>
2022-01-10 18:42:52 -0500

    creaacion proyecto

---
+++ ConcursoSofka/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+Console.WriteLine("ingrese valor a imprimir");
+
+var x = Console.ReadLine();
+
+Console.WriteLine($"el valor q ingresaste es: {x}");
+Console.WriteLine("el valor q ingresaste es: " + x);
